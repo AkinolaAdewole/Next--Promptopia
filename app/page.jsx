41a2017e14de86c7c2c530e88bd2275b3522bc6b
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-10 21:26:18 +0100

    Promptopia

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
@@ -12,7 +13,11 @@ const Home = () => {
             Promptopia is an open-source AI prompting tool for modern world to 
             discover, create and share creative prompts
         </p>
+
+        <Feed />
     </section>
+
+   
   )
 }
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
 
 const Nav = () => {
 

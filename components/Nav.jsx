b315cc0d32dec6279d1b981a6dc840faba33f9bf
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-10 21:14:23 +0100

    Promptopia

---
+++ components/Nav.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
+const Nav = () => {
+
+    const [providers, setProviders] = useState(null);
+    const [toggleDropDown, setToggleDropDown] = useState(false);
+
+    useEffect(()=>{
+        (async()=>{
+        })
+    })
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav

c0d63e4e0a81b13203d3a281a8573b659c219080
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-10 22:07:14 +0100

    Promptopia

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useEffect, useState } from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
@@ -5,16 +7,40 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 
 const Nav = () => {
-
+    
+    const {data : session} = useSession();
+    const isUserLoggedIn  = true;
+    
     const [providers, setProviders] = useState(null);
-    const [toggleDropDown, setToggleDropDown] = useState(false);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(()=>{
         (async()=>{
+          const res = await getProviders();
+          setProviders(res);
         })
-    })
+    },[]);
+
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image
+        src='/assets/images/logo.svg'
+        alt='logo'
+        width={30}
+        height={30}
+        className='object_contain'
+         />
+         <p className='logo_text'>Promptopia</p>
+      </Link>
+
+      {/*Mobile Navigation */}
+      <div className='sm:flex hidden'></div>
+    </nav>
+
+    // mobile diveces
+
+
   )
 }
 

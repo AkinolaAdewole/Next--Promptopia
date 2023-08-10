627c23f850955db63fe0e77f82b118d3ecd4ecf6
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-11 00:18:05 +0100

    Promptopia

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -75,8 +75,30 @@ const Nav = () => {
 
       {/**Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {}
-      </div>
+        {isUserLoggedIn ? (
+         <div className='flex'>
+           <Image
+              src='/assets/images/logo.svg'
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='profile'
+          />
+         </div>
+        ):(
+         <>
+             {providers && Object.values(providers).map((provider)=>(
+            <button
+              type='button'
+              key={provider.name}
+              onClick={()=>{signIn(provider.id)}}
+              className='black_btn'
+              >
+                Sign in
+              </button>
+            ))}
+          </>
+        )} </div>
 
     </nav>
 

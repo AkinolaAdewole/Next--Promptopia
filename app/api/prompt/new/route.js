e8a3f5cb43029a18d06aa618e4978e5ae0c843ca
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-11 23:04:34 +0100

    Create Post

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,10 +1,12 @@
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
 
 export const POST = async (req)=>{
     const {userId, prompt, tag} = await req.json();
 
     try {
         await connectToDB();
+        const newPrompt = new Prompt({ creator: userId, prompt, tag });
     } catch (error) {
         return new Response("Failed to create a new prompt", {status:500})
     }

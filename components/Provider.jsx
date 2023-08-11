b553ecc902f77239956728cd9c6689bbc520b0e6
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-11 12:27:54 +0100

    Tailwind CSS

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -2,13 +2,11 @@
 
 import { SessionProvider } from "next-auth/react";
 
-const Provider = ({ children, session }) => {
+const Provider = ({ children, session }) => (
   <SessionProvider session={session}>
     {children}
   </SessionProvider>
-  return (
-    <div>Provider</div>
-  )
-}
+ 
+)
 
 export default Provider

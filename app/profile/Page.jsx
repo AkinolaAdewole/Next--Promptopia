938e35dfa4e252e3d28f8e4b260d48f3ab6e17ad
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-12 22:56:57 +0100

    Edit prompt and delete prompt

---
+++ app/profile/Page.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+
+import Profile from "@components/Profile"
+
+const MyProfile = () => {
+  return (
+    <div>Parofile</div>
+  )
+}
+
+export default MyProfile;

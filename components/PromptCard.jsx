b286925fdca9529e1cba799e71b2f55700deac1c
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-12 18:06:38 +0100

    create prompt and display prompt

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -18,8 +18,12 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
     if(post.creator._id === session?.user.id)
       return router.push("/profile");
       router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
-  
+  }
 
+  const handleCopy =()=>{
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(()=>{setCopied(false), 3000})
   }
 
   return (

da1cd1b3f9b884cde147db0545a5746b60628929
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-12 17:13:44 +0100

    create prompt and display prompt

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,20 @@ import { usePathname, useRouter } from "next/navigation"
 
 const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const {data : session} = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const [copied, setCopied] = useState("");
+
+  const handleProfileClick = () =>{
+    console.log(post);
+
+    if(post.creator._id === session?.user.id){
+      return router.push("/profile");
+    }
+
+  }
+
   return (
     <div>PromptCard</div>
   )

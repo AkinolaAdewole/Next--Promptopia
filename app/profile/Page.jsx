91c6220f008785fa4be8b06e443132f31ca85151
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-13 00:14:51 +0100

    Edit prompt, delete prompt and update prompt

--- app/profile/Page.jsx
+++ app/profile/Page.jsx
@@ -48,7 +48,13 @@ const MyProfile = () => {
     }; 
 
   return (
-    <div>Profile</div>
+    <Profile
+        name='My'
+        desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+        data={myPosts}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+  />
   )
 }
 

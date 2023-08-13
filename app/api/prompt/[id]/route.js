e0299f360ff0ee56e307f8932d852af89b563b84
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-13 22:34:37 +0100

    Edit and Delete Prompt

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -42,3 +42,16 @@ export const PATCH = async(request, { params })=>{
         return new Response("Error updating prompt", { status : 500});
     }
 };
+
+export const DELETE = async (request, { params })=>{
+    try {
+        await connectToDB();
+
+        // Find the prompt by ID and remove it
+        await Prompt.findByIdAndRemove(params.id);
+
+        return new Response("Prompt deleted successfully", { status : 200});
+    } catch (error) {
+        return new Response ("Error deleting prompt", { status : 500});
+    }
+}

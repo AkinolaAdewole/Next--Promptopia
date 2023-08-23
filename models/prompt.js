fd9fcfc18554570e0ce91cf99dfab0cb174a4fb4
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-23 09:57:55 +0100

    Promptopia

--- models/prompt.js
+++ models/prompt.js
@@ -4,7 +4,7 @@ import { Schema, model, models } from 'mongoose';
 const PromptSchema= new Schema ({
     creator:{
         type:Schema.Types.ObjectId,
-        ref:"Promptopia",
+        ref:"User",
     },
     prompt:{
         type:String,
